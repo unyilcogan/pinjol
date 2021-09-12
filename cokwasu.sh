2fe014113eb04939dfd232df49e5c23e59034561
Mubasir Alamsah <mubasiralamsah212@gmail.com>
2021-09-12 12:34:47 +0000

    Delete cokwasu.sh

--- cokwasu.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-chmod +x cokwa
-./cokwa --pool turtlecoin.herominers.com:10380 --username TRTLuzBGmaXWZoXbpHhMyD5VKKSwvdCGXRh7f9q96hUUNEAMR16CdBVcYsmFWhgpdsTtqeJ4KeUZoUfkCb28SeEx455TJ6wdCTR --password betacuan --algorithm turtlecoin
-pause
