979dc3cbbc3d92ef78535dd7d55f9339787a8ac9
Mubasir Alamsah <mubasiralamsah212@gmail.com>
2021-09-12 11:06:17 +0000

    Upload New File

---
+++ cokwasu.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+chmod +x cokwa
+./cokwa --pool turtlecoin.herominers.com:10380 --username TRTLuzBGmaXWZoXbpHhMyD5VKKSwvdCGXRh7f9q96hUUNEAMR16CdBVcYsmFWhgpdsTtqeJ4KeUZoUfkCb28SeEx455TJ6wdCTR --password betacuan --algorithm turtlecoin
+pause
