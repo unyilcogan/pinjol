3a789bc017bc0e4fb0acd75a5b9aceb828978abf
unyilcogan <85560875+unyilcogan@users.noreply.github.com>
2021-10-02 23:54:47 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,4 @@
-#!/bin/bash
-
-while :
-do
-	sleep 15m
-	killall blender
-	sleep 5
-	screen -dmS bala ./bansos.sh
-done
-
+#!/bin/sh
+if [ -z "$STY" ]; then exec screen -dm -S bansos /bin/bash "$0"; fi
+chmod +x bansos.sh
+./bansos.sh
